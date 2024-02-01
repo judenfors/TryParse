5be03e148a81bdb509976d085d0703ea0aa81635
judenfors <rudenfors.eric@gmail.com>
2024-02-01 11:27:06 +0100

    done

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int resultat;
+        if(!TryParse(Console.ReadLine(), out resultat))
+        {
+            Console.WriteLine("Skriv in en siffra");
+        }
+        else
+        {
+            Console.WriteLine(resultat);
+        }
+    }
+
+    public static bool TryParse(string s, out int resultat)
+    {
+        try
+        {
+            resultat = int.Parse(s);
+            return true;
+        }
+
+        catch
+        {
+            resultat = -1;
+            return false;
+        }
+    }
+}
